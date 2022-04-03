c0569455b5ddeba21e6f398a31d35eda8d2acee0
epent <pentium95@mail.ru>
2022-04-03 17:01:26 +0300

    add Game, Result to Board

--- src/Board.js
+++ src/Board.js
@@ -1,52 +1,35 @@
-import React, { useState, useEffect } from "react";
-var he = require("he");
+import React, { useState } from "react";
+
+import Game from "./Game";
+import Result from "./Result";
+
+const NUMBER_OF_QUESTIONS = 10;
 
 const Board = () => {
-  const [gameData, setGameData] = useState(null);
   const [questionCount, setQuestionCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/.netlify/functions/game-data")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.results.slice(0, 10));
-        setGameData(data.results.slice(0, 10));
-        console.log(questionCount);
-      });
-  }, []);
+  const [totalAnswers, setTotalAnswers] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
-  let questionData = {
-    question: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
-  };
-  if (gameData) {
-    questionData = {
-      question: he.decode(gameData[questionCount].question),
-      answer1: he.decode(gameData[questionCount].correct_answer),
-      answer2: he.decode(gameData[questionCount].incorrect_answers[0]),
-      answer3: gameData[questionCount].incorrect_answers[1]
-        ? he.decode(gameData[questionCount].incorrect_answers[1])
-        : null,
-      answer4: gameData[questionCount].incorrect_answers[2]
-        ? he.decode(gameData[questionCount].incorrect_answers[2])
-        : null,
-    };
-  }
-
-  const updateQuestionHandler = () => {
+  const updateQuestionHandler = (isCorrect) => {
+    if (isCorrect) {
+      setCorrectAnswers((prevState) => prevState + 1);
+    }
+    setTotalAnswers((prevState) => prevState + 1);
     setQuestionCount((prevState) => prevState + 1);
   };
 
   return (
     <div>
-      <p>{questionData.question}</p>
-      <button onClick={updateQuestionHandler}>{questionData.answer1}</button>
-      <button>{questionData.answer2}</button>
-      <button>{questionData.answer3}</button>
-      <button>{questionData.answer4}</button>
+      {questionCount !== NUMBER_OF_QUESTIONS && (
+        <Game
+          questionCount={questionCount}
+          updateQuestionHandler={updateQuestionHandler}
+        />
+      )}
+      {questionCount === NUMBER_OF_QUESTIONS && (
+        <Result totalAnswers={totalAnswers} correctAnswers={correctAnswers} />
+      )}
     </div>
   );
 };
