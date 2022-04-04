5b3b324de728b961d1a588c4e66205c7c6c92d7d
epent <pentium95@mail.ru>
2022-04-04 16:03:59 +0300

    add mui to Timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from "react";
 
+import { Typography, Paper, Box, Grid } from "@material-ui/core";
+import TimerIcon from "@material-ui/icons/Timer";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: "#fafafa",
+    borderRadius: 10,
+    height: "300px",
+    width: "200px",
+  },
+}));
+
 const Timer = (props) => {
+  const classes = useStyles();
+
   const [countDown, setCountDown] = useState(0);
   const [runTimer, setRunTimer] = useState(false);
 
   useEffect(() => {
-    let timer;
+    let timerId;
 
     if (runTimer) {
-      setCountDown(5);
-      timer = setInterval(() => {
+      setCountDown(15);
+      timerId = setInterval(() => {
         setCountDown((countDown) => countDown - 1);
       }, 1000);
     } else {
-      clearInterval(timer);
+      clearInterval(timerId);
     }
-    return () => clearInterval(timer);
+    return () => clearInterval(timerId);
   }, [runTimer, props.startTimer]);
 
   useEffect(() => {
@@ -29,10 +44,33 @@ const Timer = (props) => {
     setRunTimer(true);
   }, [props.startTimer]);
 
+  let timer;
+  countDown >= 10 ? (timer = `00:${countDown}`) : (timer = `00:0${countDown}`);
+
   return (
-    <div>
-      <p>{`${countDown} sec`}</p>
-    </div>
+    <Paper elevation={3} className={classes.paper}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ height: 300 }}
+      >
+        <Box p={1}>
+          <Typography variant="h4" align="center">
+            Timer
+          </Typography>
+        </Box>
+        <Box p={1}>
+          <TimerIcon color="primary" style={{ fontSize: 80 }} />
+        </Box>
+        <Box p={1}>
+          <Typography variant="h3" align="center">
+            {timer}
+          </Typography>
+        </Box>
+      </Box>
+    </Paper>
   );
 };
 
