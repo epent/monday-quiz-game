d7928a6e72e98c05efe84e8dbb5b8b2e2cc21d72
epent <pentium95@mail.ru>
2022-04-07 19:16:40 +0300

    add flexbox to Score

--- src/Score.js
+++ src/Score.js
@@ -3,15 +3,27 @@ import React from "react";
 import { Paper, Box } from "@material-ui/core";
 import CloudRoundedIcon from "@material-ui/icons/CloudRounded";
 import CloudOutlinedIcon from "@material-ui/icons/CloudOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    flexGrow: 1,
+    alignSelf: "center",
+    textAlign: "center",
+    padding: theme.spacing(1),
+  },
+}));
 
 const Score = (props) => {
+  const classes = useStyles();
+
   const clouds = props.scoreList.map((elm, i) => {
     return elm === "correct" ? (
-      <Box p={1}>
+      <Box className={classes.box}>
         <CloudRoundedIcon style={{ fontSize: 40 }} color="secondary" />
       </Box>
     ) : (
-      <Box p={1}>
+      <Box className={classes.box}>
         <CloudOutlinedIcon
           style={{ fontSize: 40 }}
           color={props.questionCount === i ? "primary" : "secondary"}
@@ -22,7 +34,14 @@ const Score = (props) => {
 
   return (
     <Paper elevation={3}>
-      <Box display="flex">{clouds}</Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+      >
+        {clouds}
+      </Box>
     </Paper>
   );
 };
