20d4bd52d2470f5c726ddffd63c3db99d4039e59
epent <pentium95@mail.ru>
2022-04-09 18:12:10 +0300

    refactor images

--- src/images/index.js
+++ src/images/index.js
@@ -1,9 +1,23 @@
-export { default as general } from "./General.jpeg";
-export { default as math } from "./Math.webp";
-export { default as books } from "./Books.webp";
-export { default as film } from "./Film.jpeg";
-export { default as geography } from "./Geography.jpeg";
-export { default as history } from "./History.jpeg";
-export { default as music } from "./Music.jpeg";
-export { default as sports } from "./Sports.jpeg";
-export { default as videogames } from "./VideoGames.jpeg";
+import { default as general } from "./General.jpeg";
+import { default as math } from "./Math.webp";
+import { default as books } from "./Books.webp";
+import { default as film } from "./Film.jpeg";
+import { default as geography } from "./Geography.jpeg";
+import { default as history } from "./History.jpeg";
+import { default as music } from "./Music.jpeg";
+import { default as sports } from "./Sports.jpeg";
+import { default as videogames } from "./VideoGames.jpeg";
+
+const images = {
+  "General Knowledge": general,
+  Mathematics: math,
+  Books: books,
+  Film: film,
+  Geography: geography,
+  History: history,
+  Music: music,
+  Sports: sports,
+  "Video Games": videogames,
+};
+
+export { images as default };
